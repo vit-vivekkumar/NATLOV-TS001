118d374abffa6ef230d6ed03d284e8c9e3974435
Vivek Kumar <vivek964800@gmail.com>
2024-12-18 14:17:27 +0530

    add gitignore file

---
+++ students/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+router.register('departments', DepartmentViewSet)
+router.register('courses', CourseViewSet)
+router.register('semesters', SemesterViewSet)
+router.register('students', StudentViewSet)
+router.register('enrollments', EnrollmentViewSet)
+router.register('exams', ExamViewSet)
+router.register('results', ResultViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
