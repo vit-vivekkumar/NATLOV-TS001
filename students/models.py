991e413a08f449cef846ce9319ec4218ce32eca2
Vivek Kumar <vivek964800@gmail.com>
2024-12-18 13:43:01 +0530

    add updated code

--- students/models.py
+++ students/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Semester(models.Model):
+    number = models.IntegerField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.number} - {self.year}"
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    enrollment_number = models.CharField(max_length=15, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    date_enrolled = models.DateField(auto_now_add=True)
+
+class Exam(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    marks_obtained = models.FloatField()
+    max_marks = models.FloatField()
---
+++ students/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Department, Course, Semester, Student, Enrollment, Exam, Result
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class SemesterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Semester
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = '__all__'
